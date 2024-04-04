2d3b7ccb0213630b399b3aef3fc0135bee8f274f
my-amtech <ammuftau74@gmail.com>
2024-04-04 15:00:58 +0100

    Fix edit business

--- src/components/BusinessCard.tsx
+++ src/components/BusinessCard.tsx
@@ -1,26 +1,7 @@
-import { useState, useRef, Suspense } from 'react';
+import {Suspense } from 'react';
 
 const Card = ({ data }) => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [thumbnail, setThumbnail] = useState<string | null>(null);
-
-  const captureThumbnail = () => {
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
-
-    if (video && canvas) {
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        // Convert canvas image to data URL
-        const dataURL = canvas.toDataURL();
-        console.log(dataURL);
-        setThumbnail(dataURL);
-      }
-    }
-  };
+ 
   return (
     <div className="rounded-sm border border-stroke bg-white py-2 px-3 shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="size-[400px]">
